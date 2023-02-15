a67186e2ab8d86ed2ac09082ab427b6be06b53d9
Yuyu Lin <yuyu.lin@sas.com>
2023-02-15 16:20:04 +0100

    update version

--- dags/k8s_pod.py
+++ dags/k8s_pod.py
@@ -34,7 +34,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 with DAG(
-    'python_kubernetes_workflow_02',
+    'python_kubernetes_workflow_03',
     default_args=default_args,
     description='python_kubernetes_workflow',
     schedule_interval=timedelta(days=1),
