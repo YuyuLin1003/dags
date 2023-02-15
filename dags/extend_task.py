4dbc6df73bfc9e4f0ccb0d52e7d924a504dc7a70
Yuyu Lin <yuyu.lin@sas.com>
2023-02-15 11:46:53 +0100

    reduce the list to 1--10

--- dags/extend_task.py
+++ dags/extend_task.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 
-with DAG(dag_id="example_dynamic_task_mapping_02", start_date=datetime(2023, 2, 15)) as dag:
+with DAG(dag_id="example_dynamic_task_mapping_03", start_date=datetime(2023, 2, 15)) as dag:
 
     @task
     def add_one(x: int):
@@ -17,5 +17,5 @@ with DAG(dag_id="example_dynamic_task_mapping_02", start_date=datetime(2023, 2, 
         print(f"Total was {total}")
         return total
 
-    added_values = add_one.expand(x=range(1,100))
+    added_values = add_one.expand(x=range(1,10))
     sum_it(added_values)
