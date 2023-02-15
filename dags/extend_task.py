9d5d875b8f67e2df01bb67052ae3b39c75cfbb6a
Yuyu Lin <yuyu.lin@sas.com>
2023-02-15 11:10:42 +0100

    update extend task

--- dags/extend_task.py
+++ dags/extend_task.py
@@ -12,10 +12,10 @@ with DAG(dag_id="example_dynamic_task_mapping_01", start_date=datetime(2023, 2, 
         return x + 1
 
     @task
-    def sum_it(values,ti):
+    def sum_it(values):
         total = sum(values)
         print(f"Total was {total}")
-        ti.xcom_push(key='total', value=total)
+        return total
 
     added_values = add_one.expand(x=[1, 2, 3])
     sum_it(added_values)
